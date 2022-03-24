e3fbb27897144b27e03b385f4761a8a5c5fde2ce
anacr2002 <72752737+anacr2002@users.noreply.github.com>
2022-03-24 13:27:05 +0000

    pacman

---
+++ Computação_EX/Assets/Scripts/OnOff.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOff : MonoBehaviour
+{
+    public  GameObject pellets;
+    public float sec= 1f;
+   
+
+    void Start()
+    {
+    
+
+    }
+    }
+     
+    
+
